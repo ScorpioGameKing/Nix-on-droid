afbdc6e0e2cf993e7c83a84242b353df2ef37697
ScorpioGK <scorpiogameking@gmail.com>
2026-07-10 19:02:08 +0000

    still unable to add y functional alias for yazi

--- home.nix
+++ home.nix
@@ -31,15 +31,12 @@
   # Bash Settings
   programs.bash = {
     enable = true;
-    initExtra = ''
-      function y() {
-        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-        command yazi "$@" --cwd-file="$tmp"
-        IFS= read -r -d "" cwd < "$tmp"
-        [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
-        rm -f -- "$tmp"
-      }
-    '';
+  };
+
+  #Yazi Settings
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true;
   };
 
   # Handwritten scripts and Configs
