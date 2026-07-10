ef9c0bb820007232a126619ea6e311b827c5af29
ScorpioGK <scorpiogameking@gmail.com>
2026-07-10 14:42:39 -0400

    postion correct, trying escape single quotes

--- home.nix
+++ home.nix
@@ -35,7 +35,7 @@
       function y() {
         local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
         command yazi "$@" --cwd-file="$tmp"
-        IFS= read -r -d "" cwd < "$tmp"
+        IFS= read -r -d \'\' cwd < "$tmp"
         [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
         rm -f -- "$tmp"
       }
