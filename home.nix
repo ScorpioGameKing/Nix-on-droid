40f0bca1cc48cd671f94446ddecd26c8f295c4c1
ScorpioGK <scorpiogameking@gmail.com>
2026-07-10 14:16:22 -0400

    testing bash options

--- home.nix
+++ home.nix
@@ -31,6 +31,15 @@
   # Bash Settings
   programs.bash = {
     enable = true;
+    bashrcExtra = ''
+      function y() {
+	      local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	      command yazi "$@" --cwd-file="$tmp"
+	      IFS= read -r -d '' cwd < "$tmp"
+	      [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	      rm -f -- "$tmp"
+      }
+    ''
   };
 
   # Handwritten scripts and Configs
