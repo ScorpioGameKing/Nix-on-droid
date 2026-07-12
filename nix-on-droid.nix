7921e1d07bce37ef4ad3436de3f54a71ef8a9aa9
ScorpioGK <scorpiogameking@gmail.com>
2026-07-11 23:33:49 -0400

    Update nix-on-droid.nix

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -11,6 +11,13 @@
 
 { config, lib, pkgs, ... }:
 
+let
+  sshdTmpDirectory = "${config.user.home}/sshd-tmp";
+  sshdDirectory = "${config.user.home}/sshd";
+  pathToPubKey = "...";
+  port = 8022;
+in
+
 {
   # Install System Packages
   environment.packages = with pkgs; [
@@ -36,7 +43,31 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+  build.activation.sshd = ''
+    $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${config.user.home}/.ssh"
+    $DRY_RUN_CMD cat ${pathToPubKey} > "${config.user.home}/.ssh/authorized_keys"
 
+    if [[ ! -d "${sshdDirectory}" ]]; then
+      $DRY_RUN_CMD rm $VERBOSE_ARG --recursive --force "${sshdTmpDirectory}"
+      $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${sshdTmpDirectory}"
+
+      $VERBOSE_ECHO "Generating host keys..."
+      $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f "${sshdTmpDirectory}/ssh_host_rsa_key" -N ""
+
+      $VERBOSE_ECHO "Writing sshd_config..."
+      $DRY_RUN_CMD echo -e "HostKey ${sshdDirectory}/ssh_host_rsa_key\nPort ${toString port}\n" > "${sshdTmpDirectory}/sshd_config"
+
+      $DRY_RUN_CMD mv $VERBOSE_ARG "${sshdTmpDirectory}" "${sshdDirectory}"
+    fi
+  '';
+  environment.packages = [
+    (pkgs.writeScriptBin "sshd-start" ''
+      #!${pkgs.runtimeShell}
+
+      echo "Starting sshd in non-daemonized way on port ${toString port}"
+      ${pkgs.openssh}/bin/sshd -f "${sshdDirectory}/sshd_config" -D
+    '')
+  ];
   # Enable Home Manager Configuration
   home-manager = {
     backupFileExtension = "hm-bak";
